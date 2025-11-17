ab0418f22f4f10b84be340005be1a7b92ee07e6d
disha <dishanayak395@gmail.com>
2025-11-17 18:20:21 +0530

    Added UserController for user registration

--- bank/src/main/java/com/example/bank/config/ProjectSecurityConfig.java
+++ bank/src/main/java/com/example/bank/config/ProjectSecurityConfig.java
@@ -4,14 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
@@ -21,14 +16,32 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ProjectSecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/notices", "/contact", "/error").permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http,
+                                                   BankUserDetailsService bankUserDetailsService) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact", "/error", "/register").permitAll()
+                )
+                .userDetailsService(bankUserDetailsService)
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+
         return http.build();
     }
+
+
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+    @Bean
+    public UserDetailsService userDetailsService(BankUserDetailsService bankUserDetailsService) {
+        return bankUserDetailsService;
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
